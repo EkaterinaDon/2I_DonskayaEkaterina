e33438166c596c131d5832b920871a54ce9aac01
Ekaterina <katrine.d@gmail.com>
2020-07-02 10:57:01 +0300

    first commit

--- 2I_DonskayaEkaterina/main.swift
+++ 2I_DonskayaEkaterina/main.swift
@@ -1,12 +1,1 @@
-//
-//  main.swift
-//  2I_DonskayaEkaterina
-//
-//  Created by Ekaterina Donskaya on 02/07/2020.
-//  Copyright © 2020 Ekaterina Donskaya. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
 
